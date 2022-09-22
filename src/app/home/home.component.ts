e41d9ea4f42268d66b795529908c7bc07053ed62
Timmo <timmo.n@itcgroup.io>
2022-09-22 11:38:39 +0700

    Responsive

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,7 +17,7 @@ export class HomeComponent implements OnInit {
 
   constructor(private dataService: DataService) {
     this.getExchangeRate();
-    this.getExchangeRateAndPut(); // Sync Exchange from API
+    // this.getExchangeRateAndPut(); // Sync Exchange from API
   }
 
   ngOnInit(): void {
